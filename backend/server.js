95d2e90b794f5fed15cdfd2b60fb17d90c132bd0
Poo306 <poojarp948@gmail.com>
2025-07-10 22:45:15 +0530

    Connecting Mongoose in server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = 5000;
+//Connect Mongoose in server.js
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
