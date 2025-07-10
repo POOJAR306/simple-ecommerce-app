292370bc30b1c38e438d29d5db39704cabc06883
Poo306 <poojarp948@gmail.com>
2025-07-10 23:16:07 +0530

    updated import modules, mongodb models and setup app in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,22 @@
-// ✅ Import modules
+// Import modules
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require("dotenv").config();
 
-// ✅ Import MongoDB models
+// Import MongoDB models
 const Product = require('./models/Product');
 const CartItem = require('./models/CartItem');
 
-// ✅ Setup app
+// Setup app
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 //Connect Mongoose in server.js
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("✅ MongoDB Connected"))
-  .catch(err => console.error("❌ Mongo Error", err));
+  .then(() => console.log(" MongoDB Connected"))
+  .catch(err => console.error(" Mongo Error", err));
 
 // app.use(cors());
 // app.use(express.json());
