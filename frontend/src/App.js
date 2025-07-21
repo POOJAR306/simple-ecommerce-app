a7d9f51c028d56b994ddfe569607185ad066e8aa
Poo306 <poojarp948@gmail.com>
2025-07-21 22:47:58 +0530

    modified App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,11 @@
-import React, { useState } from 'react';
-import ProductList from './ProductList';
-import Cart from './Cart';
+import React from 'react';
+import ProductList from './components/ProductList';
 
 function App() {
-  const [cartItems, setCartItems] = useState([]);
-
-  const addToCart = (product) => {
-    setCartItems([...cartItems, product]);
-  };
-
   return (
     <div>
-      <h1>🛒 Ecommerce Dashboard</h1>
-      <ProductList addToCart={addToCart} />
-      <Cart cartItems={cartItems} />
+      <h1>My E-Commerce App</h1>
+      <ProductList />
     </div>
   );
 }
