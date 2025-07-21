eb6817beb0c3479d29386053e8da36583f72fe61
Poo306 <poojarp948@gmail.com>
2025-07-21 18:55:47 +0530

    added  changes in const cartItemSchema

--- backend/models/cartitem.js
+++ backend/models/cartitem.js
@@ -1,15 +1,8 @@
 const mongoose = require("mongoose");
 
-const CartItemSchema = new mongoose.Schema({
-    productId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Product',
-    required: true
-  },
-  quantity: {
-    type: Number,
-    default: 1
-  }
+const cartItemSchema = new mongoose.Schema({
+  productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+  quantity: Number
 });
 
 module.exports = mongoose.model("CartItem", CartItemSchema);
