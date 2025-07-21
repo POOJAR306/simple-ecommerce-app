a09267cf152c08730b87e9b8b9947c45a4abe45f
Poo306 <poojarp948@gmail.com>
2025-07-21 19:11:38 +0530

    changed product to productId

--- backend/models/cartitem.js
+++ backend/models/cartitem.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const cartItemSchema = new mongoose.Schema({
-  product: {
+  productId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Product",
     required: true,
