8a3e20cf7a104e1d1147cd5aaa2094357d7cabf2
Poo306 <poojarp948@gmail.com>
2025-07-21 22:53:00 +0530

    Updated App.js to Include AddProduct

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import AddProduct from './components/AddProduct';
 import ProductList from './components/ProductList';
 
 function App() {
   return (
     <div>
       <h1>My E-Commerce App</h1>
+      <AddProduct />
       <ProductList />
     </div>
   );
